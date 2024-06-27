adbfe8d74ef085436713c059d5836433b1cdfd71
L. <64107059+Dev-L0@users.noreply.github.com>
2024-06-27 13:06:59 +0000

    ..

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ const skills = document.querySelectorAll(".skills");
 const tools = document.querySelectorAll(".tools");
 const skillsToolsToggle = document.querySelector(".skills-tools-toggle");
 const toggleTheme = document.querySelector(".theme-toggle-inp");
-const navbar = document.querySelector("navbar");
-
+const logo =  document.querySelector(".logo");
+const h3 = document.querySelectorAll("h3");
 
 
 const inActive = (el) => {
@@ -37,13 +37,39 @@ toggleTheme.addEventListener("change", function switchTheme(){
 
 if(toggleTheme.checked === true){
  document.body.classList.add('light');
-
-  
 }
  else{
   document.body.classList.remove('light');
- 
  }
   
- 
 })
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  let arr = [];
+function blink() {
+  
+  setInterval(() => {
+    for(let i = 0; i < 3; i++) {
+      const random = Math.floor(Math.random() * 256 + 1);
+      arr.push(random);
+    }
+    console.log(arr);
+    let rgbColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+    logo.style.color= rgbColor;
+  
+
+     arr = []; 
+
+  }, 3000); 
+}
+blink();
+});
+
+
+
+
+
+
+
